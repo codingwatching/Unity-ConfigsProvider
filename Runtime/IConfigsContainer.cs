45fc44b4d981372e247cfed6d443939a524a2d6c
CoderGamester <game.gamester@gmail.com>
2023-09-03 23:34:01 +0100

    Replace bad struct objects to classes Add missing documentation

--- Runtime/IConfigsContainer.cs
+++ Runtime/IConfigsContainer.cs
@@ -17,7 +17,7 @@ namespace GameLovers.ConfigsProvider
 	/// Generic container of the configs imported with a ConfigsImporter script
 	/// The given <typeparamref name="T"/> type is the same of the config struct defined to be serialized in the scriptable object
 	/// </summary>
-	public interface IConfigsContainer<T> where T : struct
+	public interface IConfigsContainer<T>
 	{
 		List<T> Configs { get; set; }
 	}
@@ -26,7 +26,7 @@ namespace GameLovers.ConfigsProvider
 	/// Generic container of the unique single config imported with a ConfigsImporter script
 	/// The given <typeparamref name="T"/> type is the same of the config struct defined to be serialized in the scriptable object
 	/// </summary>
-	public interface ISingleConfigContainer<T> where T : struct
+	public interface ISingleConfigContainer<T>
 	{
 		T Config { get; set; }
 	}
@@ -36,7 +36,16 @@ namespace GameLovers.ConfigsProvider
 	/// Use this configs container to hold the configs data in pairs mapped by it's given <typeparamref name="TKey"/> id
 	/// </remarks>
 	public interface IPairConfigsContainer<TKey, TValue> : IConfigsContainer<Pair<TKey, TValue>>
+	{
+	}
+
+	/// <inheritdoc />
+	/// <remarks>
+	/// Use this configs container to hold the configs data in pairs mapped by it's given <typeparamref name="TKey"/> id
+	/// </remarks>
+	public interface IStructPairConfigsContainer<TKey, TValue> : IConfigsContainer<StructPair<TKey, TValue>>
 		where TKey : struct
+		where TValue : struct
 	{
 	}
 }
